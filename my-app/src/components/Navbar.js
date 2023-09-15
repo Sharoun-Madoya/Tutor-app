5e83ca0cdc370287e6b00dc64e711602213289b9
[sharoun <[madoyasharoun@gmail.com]>
2023-09-15 19:39:21 +0300

    Added links, display works

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -5,10 +5,13 @@ import TutorialModule from './tutorial';
 
 function Navbar (){
     return(
-        <div className="container">
-            <DomainModule/>
-            <StudentModule />
-            <TutorialModule />
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Link exact to="/">Home</Link>
+        <Link to= "/student">Student Module</Link>
+        <Link to= "/domain">Domain Module</Link>
+        <Link to="/tutorial">Tutorials</Link>
+        </nav>
     )
 }
+
+export default Navbar;
